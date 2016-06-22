525db8451f244adf28af1081f5627d2ce655c42d
Michael Brumlow <mike@r1soft.com>
2016-06-22 14:34:43 -0500

    Updated install script.

--- install.sh
+++ install.sh
@@ -2,12 +2,24 @@
 
 cd skel
 
-for i in `git ls-files`; do
+for i in `ls -a`; do
+
+    if [ $i == "." ]; then
+	continue
+    fi
+
+    if [ $i == ".." ]; then
+	continue
+    fi
 
     echo "Installing $i"
 
     if [ -f $i ]; then
         ln -f $i ~/$i
+    fi
+
+    if [ -d $i ]; then
+        ln -sf `pwd`/$i ~/$i
     fi
 
 done
